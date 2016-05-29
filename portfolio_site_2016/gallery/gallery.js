ed81ad5312d14f99b675529d9294770a96dbd473
Vincent Pauley <vinniepauley@gmail.com>
2016-05-29 17:41:12 -0500

    completed gallery for now

--- portfolio_site_2016/gallery/gallery.js
+++ portfolio_site_2016/gallery/gallery.js
@@ -1,8 +1,14 @@
+
+/* Add images and descriptions as JSON objects in this array, the following code handles everything else*/
 var images = [
-  {'imageLocation' : 'images/cross-enterprises_full.jpg'},
-  {'imageLocation' : 'images/csod_internal.jpg'},
-  {'imageLocation' : 'images/initech.jpg'},
-  {'imageLocation' : 'images/connect.png'}
+  {'imageLocation' : 'images/cross-enterprises_full.jpg',
+   'descriptionText' : 'This page was designed as a demonstration to clients as to what was possible to create in the Cornerstone system. For confidentiality reasons I cannot share actual client pages but this page shows many of the design principles used during my time as an independent contractor.'},
+  {'imageLocation' : 'images/csod_internal.jpg',
+   'descriptionText' : 'I designed this layout to simplify a congested page that had been present within a web environment for years. The simple colors and graphics hold true to the branding guidelines for this particular company, and help ease the task of navigation.'},
+  {'imageLocation' : 'images/initech.jpg',
+   'descriptionText' : 'This page was designed as a demonstration to clients as to what was possible to create in the Cornerstone system. For confidentiality reasons I cannot share actual client pages but this page shows many of the design principles I used during my time as an independent contractor.'},
+  {'imageLocation' : 'images/connect.png',
+   'descriptionText' : 'This Mock-up was designed for a client who wanted to take advantage of new and complicated features within the system we were using. Considerable knowledge of coding and patience was needed in order to create this page within the system. Keep in mind that certain information has been blurred for confidentiality purposes.'}
 ];
 
 var currentIndex = 0;
@@ -71,14 +77,23 @@ function shiftLeft() {
   }
 }
 
+/* UPDATE FUNCTIONS */
+
 /* provide the index of what image you want and this will update the gallery */
 function updateImage(imageIndex) {
   // update global var that stores index
   currentIndex = imageIndex;
-  // get location of new image
-  var newImage = images[imageIndex].imageLocation;
-  // update the current image with the new src
-  $('#showcase-image').attr('src', newImage);
+
+  var showcaseImage = $('#showcase-image')
+  // for fade effect
+  showcaseImage.css('opacity', '0');
+  setTimeout(function() {
+    showcaseImage.css('opacity', '1');
+    // get location of new image
+    var newImage = images[imageIndex].imageLocation;
+    // update the current image with the new src
+    showcaseImage.attr('src', newImage);
+  }, 250);
   // update balls
   updateBalls();
 }
@@ -87,8 +102,24 @@ function updateImage(imageIndex) {
 function updateBalls() {
   displayedImageBall = '#index_ball' + currentIndex;
   $(displayedImageBall).css('background-color', '#FFF').siblings().css('background-color', '#9C9C9C');
+  updateText();
+}
+/*
+function updateText() {
+  $('#description-text > p').html(images[currentIndex].descriptionText);
+}*/
+
+function updateText() {
+  var descriptionText = $('#description-text > p');
+
+  descriptionText.css('opacity', '0');
+  setTimeout(function() {
+    descriptionText.css('opacity', '1');
+    descriptionText.html(images[currentIndex].descriptionText);
+  }, 250);
 }
 
+// INIT
 initializeGallery();
 
 /* ALL DONE!
